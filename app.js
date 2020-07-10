29abbdd14a6df4d4bf9411699fb1b1c271e30929
rudyleiva93 <leivarudy93@gmail.com>
2020-07-10 10:08:32 -0700

    Locate current user via public ip address

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const iplocate = require("node-iplocate");
+const publicIp = require("public-ip");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,26 +13,48 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  const query = req.body.cityName;
-  const apiKey = req.body.apiKey;
-  const units = req.body.units;
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + units;
-  console.log(url);
-  https.get(url, function(response) {
-    console.log(response.statusCode);
 
-    response.on("data", function(data) {
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcuis</h1>");
-      res.write("<p>The weather is currently " + weatherDescription + ".</p>");
-      res.write("<img src= " + imageURL +">");
-      res.send();
+
+  if(req.body.hasOwnProperty("currentLocation")) {
+
+    (async () => {
+
+      console.log(await publicIp.v4());
+
+      var ipv6 = await publicIp.v6();
+      iplocate(ipv6).then(function(results) {
+        console.log("IP Address: " + results.ip);
+        console.log("Country: " + results.country + " (" + results.country_code + ")");
+        console.log("Continent: " + results.continent);
+        console.log("Organisation: " + results.org + " (" + results.asn + ")");
+
+        console.log(JSON.stringify(results, null, 2));
+      });
+
+    })();
+  }
+  else {
+    const query = req.body.cityName;
+    const apiKey = req.body.apiKey;
+    const units = req.body.units;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + units;
+    console.log(url);
+    https.get(url, function(response) {
+      console.log(response.statusCode);
+
+      response.on("data", function(data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcuis</h1>");
+        res.write("<p>The weather is currently " + weatherDescription + ".</p>");
+        res.write("<img src= " + imageURL +">");
+        res.send();
+      });
     });
-  });
+  }
 });
 
 
